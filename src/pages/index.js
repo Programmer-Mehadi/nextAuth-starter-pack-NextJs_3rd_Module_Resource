89a381d830867e8fc92fd955a7273d2a6ac77ae1
Programmer-Mehadi <mehadi.developer@gmail.com>
2023-07-24 09:39:04 +0600

    github implement.

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import { useSession } from "next-auth/react";
+const HomePage = () => {
+  const { data: session } = useSession();
+  return (
+    <div>
+      <Head>
+        <title>Next Auth</title>
+      </Head>
+      <h1 style={{ textAlign: "center", marginTop: "10%" }}>Welcome To Next Auth Home Page</h1>
+      <h2 style={{ textAlign: "center", marginTop: "10px" }}>{session?.user?.name}</h2>
+    </div>
+  );
+};
+
+export default HomePage;
